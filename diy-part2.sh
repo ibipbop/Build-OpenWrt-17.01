808b12162f48c199facd3dce5368d010efdf37d9
ibipbop <81741593+ibipbop@users.noreply.github.com>
2021-04-20 17:28:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 rm -rf ./package/lean/luci-theme-argon
+rm -rf ./package/openwrt-passwall/v2ray-plugin
+rm -rf ./package/openwrt-passwall/v2ray
